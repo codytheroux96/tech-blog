1bdd36d6fc306299588aa6419a2bd233bcd80b6c
cdt96 <cody.theroux3@gmail.com>
2022-06-30 16:04:47 -0400

    made an update a post route

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -12,16 +12,49 @@ router.post('/', (req, res) => {
         content: req.body.content,
         user_id: req.session.user_id
     })
-    .then(newPostData => res.json(newPostData))
-    .catch((err) => {
+        .then(newPostData => res.json(newPostData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//Update a post
+router.put('/:id', (req, res) => {
+    Post.update({
+        title: req.body.title,
+        content: req.body.title,},
+        {
+            where: {
+                id: req.params.id
+            }
+        }).then(newPostData => {
+            if (!newPostData) {
+                res.status(404).json({ message: 'Error updating message!' })
+                return;
+            }
+            res.json(newPostData)
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+//Delete a post
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(newPostData => {
+        if (!newPostData) {
+            res.status(404).json({ message: 'No post found!' })
+            return;
+        }
+        res.json(newPostData)
+    }).catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
-
-//Update a post
-
-//Delete a post
-
 
 module.exports = router;
