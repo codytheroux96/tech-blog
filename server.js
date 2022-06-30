3b1a771f630cf6830e393f439441a730258e1bf9
cdt96 <cody.theroux3@gmail.com>
2022-06-30 11:20:52 -0400

    brought in dependencies for handlebars

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
