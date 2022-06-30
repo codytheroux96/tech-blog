f9eb868bac81db9d999e37e5dd73d56cf936fabd
cdt96 <cody.theroux3@gmail.com>
2022-06-30 09:42:43 -0400

    filled out my index.js in models

--- models/index.js
+++ models/index.js
@@ -2,7 +2,29 @@ const User = require('./users');
 const Post = require('./post');
 const Comment = require('./comments');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsToMany(User,{
+    foreignKey: 'user_id'
+})
+
+Comment.belongsToMany(Post, {
+    foreignKey: 'post_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 
 
 module.exports = { User, Post, Comment };
