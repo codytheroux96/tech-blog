95bff8dbdf8ad7b59ea7eace7b2d33ca6c90c621
cdt96 <cody.theroux3@gmail.com>
2022-06-30 15:44:09 -0400

    made a get all comments route and a create a comment route

--- routes/api/commentRoutes.js
+++ routes/api/commentRoutes.js
@@ -1,9 +1,29 @@
 const router = require('express').Router();
 const { Comment, User, Post } = require('../../models');
 //Create a comment
+router.post('/', (req, res) => {
+    Comment.create({
+        text: req.body.text,
+        post_id: req.body.post_id,
+        user_id: req.body.user_id
+    })
+        .then(newCommentData => res.json(newCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 //Get all comments
 
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(newCommentData => res.json(newCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
 
 module.exports = router;
